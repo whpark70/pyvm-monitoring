1e8502d2743c3b4e71d23f6df2c8ea3e487e82a1
whpark70 <whpark70@gmail.com>
2017-12-20 14:00:24 +0900

    추가

---
+++ mvariables.py
@@ -0,0 +1,5 @@
+# Counter id - description
+counterId_Desc = {6:'cpu usage', 70: 'mem.swapped.average', 125:'disk.usage.average', 131:'disk.write.average' }
+
+# plot 시 color: 회색: #808080 
+colors = {0:'blue', 1:'#808080', 2: 'green', 3:'black', 4: 'yellow', 5: 'red', 6: 'white'}
